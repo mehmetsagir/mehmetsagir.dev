7c6e0cc5820a8a963c86e3e69a769679f9643191
mehmetsagir <memetsagirint@gmail.com>
2022-04-12 00:03:37 +0300

    add AppProps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import { AppProps } from 'next/app';
 import Layout from 'src/components/Layout';
 
-function MyApp({ Component, pageProps }: any) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
