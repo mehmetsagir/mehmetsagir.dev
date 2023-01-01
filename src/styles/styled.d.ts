ec590a44b909936bb4210ef563fd8a0186fc1e2a
Mehmet <memetsagirint@gmail.com>
2023-01-01 21:22:37 +0300

    initial commit

--- src/styles/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components';
-
-import theme from './theme';
-
-type themeType = typeof theme;
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends themeType {}
-}
