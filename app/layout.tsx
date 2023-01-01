663c84747e1a2a601d0287d3bb50a26ff83c3f7d
Mehmet <memetsagirint@gmail.com>
2023-01-01 22:13:39 +0300

    style: Add tailwind and configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="max-w-3xl mx-auto p-4">{children}</div>
+      </body>
     </html>
   );
 }
