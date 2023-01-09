3a20e2dd35a9e42f5b79a80c14aac353c0a85093
Mehmet <memetsagirint@gmail.com>
2023-01-09 07:47:19 +0300

    style: improve layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,28 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body>
-        <div className="max-w-3xl mx-auto p-4">
+        <div className="max-w-3xl mx-auto py-4 px-6 lg:px-0 relative">
+          <svg
+            className="pointer-events-none fixed isolate z-50 opacity-70 mix-blend-soft-light top-0 left-0"
+            width="100%"
+            height="100%"
+          >
+            <filter id="pedroduarteisalegend">
+              <feTurbulence
+                type="fractalNoise"
+                baseFrequency="0.80"
+                numOctaves="4"
+                stitchTiles="stitch"
+              ></feTurbulence>
+            </filter>
+            <rect
+              width="100%"
+              height="100%"
+              filter="url(#pedroduarteisalegend)"
+            ></rect>
+          </svg>
           <Menu />
-          <main className="mt-10 sm:mt-16">{children}</main>
+          <main className="mt-8">{children}</main>
         </div>
       </body>
     </html>
