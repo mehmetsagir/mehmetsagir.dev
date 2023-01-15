07d118a286b079ea0deaee3395b6481c8ff80e1d
Mehmet <memetsagirint@gmail.com>
2023-01-15 03:40:21 +0300

    fix: typo

--- services/getPinnedRepositories.ts
+++ services/getPinnedRepositories.ts
@@ -1,6 +1,6 @@
 export async function getPinnedRepositories() {
   const response = await fetch(
-    `https://gh-pinned-repos.egoist.dev/?username=${process.env.GITHUB_USERNAME}}`
+    `https://gh-pinned-repos.egoist.dev/?username=${process.env.GITHUB_USERNAME}`
   );
   if (!response.ok) return [];
 
