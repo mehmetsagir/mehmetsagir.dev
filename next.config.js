f5fe22b07e1c55c319a015c484b0a5d7c6dbf7a9
Mehmet Sağır <mehmetsagir@Mehmet-MacBook-Pro.local>
2024-01-07 13:50:01 +0300

    chore: update svgr configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -1,15 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
-  },
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack']
-    });
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.('.svg')
+    );
 
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/ // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack']
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
     return config;
   }
 };
