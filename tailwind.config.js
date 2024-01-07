c95b30c2c9a4443d6ef6b802b8f6786f726354bf
Mehmet Sağır <mehmetsagir@Mehmet-MacBook-Pro.local>
2024-01-07 13:59:23 +0300

    chore: fix plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,5 @@ module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {}
-  },
-  plugins: [require('@tailwindcss/line-clamp')]
+  }
 };
